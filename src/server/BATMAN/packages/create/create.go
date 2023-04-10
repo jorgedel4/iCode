f5b9a5c73ca3c9e75f79da76adb959e01858cc93
jorgedel4 <70418841+jorgedel4@users.noreply.github.com>
2023-04-10 12:35:23 -0600

    Added functionality to create term

--- src/server/BATMAN/packages/create/create.go
+++ src/server/BATMAN/packages/create/create.go
@@ -70,6 +70,36 @@ func campus(w http.ResponseWriter, r *http.Request, mongoDB *mongo.Client, mysql
 	}
 }
 
-func term(w http.ResponseWriter, r *http.Request, mongoDB *mongo.Client, mysqlDB *sql.DB, body []byte) {
+type Term struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
 
+func term(w http.ResponseWriter, r *http.Request, mongoDB *mongo.Client, mysqlDB *sql.DB, body []byte) {
+	var term Term
+	err := json.Unmarshal(body, &term)
+	if err != nil {
+		http.Error(w, "Error decoding request body", http.StatusBadRequest)
+		return
+	}
+
+	insertQuery := fmt.Sprintf("INSERT INTO terms VALUES ('%s', '%s')", term.ID, term.Name)
+	stmt, err := mysqlDB.Prepare(insertQuery)
+	if err != nil {
+		http.Error(w, "Internal error with DB", http.StatusInternalServerError)
+		return
+	}
+	defer stmt.Close()
+
+	// Execute the query with the values
+	_, err = stmt.Exec()
+	if err != nil {
+		if strings.Contains(err.Error(), "Error 1062") {
+			http.Error(w, fmt.Sprintf("Term with id '%s' already exists", term.ID), http.StatusConflict)
+			return
+		} else {
+			http.Error(w, "Internal error with DB", http.StatusInternalServerError)
+			return
+		}
+	}
 }
