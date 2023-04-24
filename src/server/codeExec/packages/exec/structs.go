4a77204ea60dc57e2a46dc326e052e08b1edfb4e
jorgedel4 <70418841+jorgedel4@users.noreply.github.com>
2023-04-23 21:00:44 -0600

    Working

--- src/server/codeExec/packages/exec/structs.go
+++ src/server/codeExec/packages/exec/structs.go
@@ -1,13 +1,25 @@
 package exec
 
 type Problem struct {
-	Inputs     [][]string `json:"inputs"`
-	Outputs    []string `json:"outputs"`
-	TimeOutSec int `json:"timeoutSec"`
-	Language   string `json:"language"`
+	HiddenInputs   [][]string `json:"hinputs"`
+	HiddenOutputs  []string   `json:"houtputs"`
+	ShownInputs    [][]string `json:"sinputs"`
+	ShownOutputs   []string   `json:"soutputs"`
+	TimeOutSec     int        `json:"timeoutSec"`
+	Language       string     `json:"language"`
+	ForbiddenFuncs []string   `json:"forbidden_funcs"`
 }
 
 type RequestBody struct {
 	ID   string `json:"id"`
 	Code string `json:"code"`
+}
+
+type Result struct {
+	Error        string                   `json:"error"`
+	ShownTests   []map[string]interface{} `json:"shownTests"`
+	ShownPassed  int                      `json:"shownPassed"`
+	ShownFailed  int                      `json:"shownFailed"`
+	HiddenTests  map[string]int           `json:"hiddenTests"`
+	Passed       bool                     `json:"passed"`
 }
