5bcefffb89fa8f08e3d519cde19912178424bd22
Ruben Vazquez <a01735407@tec.mx>
2023-05-11 13:18:25 -0600

    [Server] RIDDLE getQuestionHw/ getQuestionMod / questionRequest

--- src/server/RIDDLE/packages/util/filterQuestions.go
+++
@@ -1,9 +0,0 @@
-package util
-
-import (
-	"net/http"
-)
-
-func FilterQuestions(w http.ResponseWriter) {
-
-}
--- src/server/RIDDLE/packages/util/hwQuestions.go
+++ src/server/RIDDLE/packages/util/hwQuestions.go
@@ -1,6 +1,6 @@
 package util
 
-/* import (
+import (
 	"database/sql"
 	"elPadrino/RIDDLE/packages/structs"
 	"fmt"
@@ -9,41 +9,44 @@ package util
 )
 
 // Funcion que reciba el id_estudiante y id_hwQuestion para asignar status a cada pregunta y de este modo definir como se muestran
-func HwQuestions(w http.ResponseWriter, req structs.SelectQuestion, mysqlDB *sql.DB) {
+func HwQuestions(w http.ResponseWriter, req structs.SelectQuestion, mysqlDB *sql.DB) ([]structs.ResultQuestion, error) {
 
 	//Permitir que cualquier origen ingrese a este recurso
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	//Query de base para preguntas de tareas
-	baseQueryHw := `SELECT q.q_type, q.info
-	FROM hw_questionAttempts hqa
-	JOIN questions q ON hqa.question = q.id_question`
+	baseQueryHw := `SELECT id_question, q_type, info, mod_question_status2(?, id_question, ?) AS status
+	FROM questions`
 
-	//Slices para almacenar los argumentos del Where
+	//Slices para almacenar los selectores y valores del where
 	var selectors []string
 	var values []interface{}
 
-	//Almacenamiento de los parametros en los argmentos de la query
-	if reqS != "all" {
-		selectors = append(selectors, "hqa.student = ?")
-		values = append(values, req.Student)
-	}
+	//
+	values = append(values, req.Assigment) //id_homework
+	values = append(values, req.Student)   // studiante
 
-	if reqA != "all" {
-		selectors = append(selectors, "q.module = ?")
-		//module en DB pero es referente a Assigment tanto para grupo como tarea
-		values = append(values, reqA)
-	}
+	selectors = append(selectors, "current_status = ?")
+	values = append(values, "APP")
 
-	//agregar al where preguntas con unn estatus en especifico
-	//En este caso a
-	selectors = append(selectors, "q.current_status = ?")
-	values = append(values, "NOP")
+	selectors = append(selectors, "module = ?")
+	values = append(values, req.Module)
+
+	//Le paso la funcion en el where para que solo me de las preguntas con PEN
+	selectors = append(selectors, "mod_question_status2(?, id_question, ?) = ? OR mod_question_status2(?, id_question, ?) = ?")
+	//Primer chequeo con PEN
+	values = append(values, req.Assigment)
+	values = append(values, req.Student)
+	values = append(values, "PEN")
+	//Segunda revision con "FAI"
+	values = append(values, req.Assigment)
+	values = append(values, req.Student)
+	values = append(values, "FAI")
 
 	//Complete the Query
 	var query string
 	if len(selectors) > 0 {
-		query = fmt.Sprintf("%s WHERE %s", baseQueryHw, strings.Join(selectors, " AND "))
+		query = fmt.Sprintf("%s WHERE %s LIMIT 1", baseQueryHw, strings.Join(selectors, " AND "))
 	} else {
 		query = baseQueryHw
 	}
@@ -52,7 +55,7 @@ func HwQuestions(w http.ResponseWriter, req structs.SelectQuestion, mysqlDB *sql
 	rows, err := mysqlDB.Query(query, values...)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return []structs.ResultQuestion{}, err
 	}
 	defer rows.Close() //Close the conection
 
@@ -61,10 +64,71 @@ func HwQuestions(w http.ResponseWriter, req structs.SelectQuestion, mysqlDB *sql
 	var results []structs.ResultQuestion
 	for rows.Next() {
 		var result structs.ResultQuestion
-		if err = rows.Scan(&result.Type, &result.Info); err != nil {
+		if err = rows.Scan(&result.IdPregunta, &result.Type, &result.Info, &result.Status); err != nil {
 			http.Error(w, "Error reading results", http.StatusInternalServerError)
-			return
+			return []structs.ResultQuestion{}, err
 		}
 		results = append(results, result)
 	}
-} */
+
+	return results, nil
+}
+
+/* /* FUnction to questions from homeworks */
+/* DELIMITER $$
+
+CREATE FUNCTION get_hwQuestion_status (in_homework CHAR(10), in_question CHAR(20), in_student CHAR(9))
+RETURNS CHAR(3)
+BEGIN
+    DECLARE out_status CHAR(3);
+    SELECT
+        CASE
+            WHEN EXISTS (
+                SELECT * FROM hw_questionAttempts
+                WHERE student = in_student AND grupo = in_homework AND question = in_question AND attempt_status = 'PAS'
+            ) THEN 'PAS'
+            WHEN EXISTS (
+                SELECT * FROM hw_questionAttempts
+                WHERE student = in_student AND grupo = in_group AND question = in_question
+            ) THEN 'FAI'
+            ELSE 'PEN'
+        END
+    INTO out_status;
+    RETURN out_status;
+END$$
+
+DELIMITER ;
+
+SHOW FUNCTION STATUS WHERE Db = 'iCode';
+
+DELIMITER $$
+CREATE FUNCTION get_hw_status2(id_task CHAR(20), question_id CHAR(20), student CHAR(9))
+RETURNS CHAR(3)
+BEGIN
+    RETURN (
+        SELECT
+            CASE
+                WHEN EXISTS (
+                    SELECT *
+                    FROM hw_questionAttempts
+                    WHERE
+                        student = get_hw_status.student AND
+                        grupo = id_task AND
+                        question = question_id AND
+                        attempt_status = 'PAS'
+                )
+                THEN 'PAS'
+                WHEN EXISTS (
+                    SELECT *
+                    FROM hw_questionAttempts
+                    WHERE
+                        student = get_hw_status.student AND
+                        grupo = id_task AND
+                        question = question_id AND
+                        attempt_status <> 'PAS'
+                )
+                THEN 'FAI'
+                ELSE 'PEN'
+            END
+        );
+END$$ */
