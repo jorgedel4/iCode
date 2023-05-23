428795cd51bc5043ae8f5b50e8e1c8a4e64a4311
jorgedel4 <70418841+jorgedel4@users.noreply.github.com>
2023-05-23 14:23:32 -0600

    [DB] Added functions for GET/hwQuestions

--- src/server/seeds/functions.sql
+++ src/server/seeds/functions.sql
@@ -28,6 +28,8 @@ BEGIN
 END$$
 DELIMITER ;
 
+
+
 DELIMITER $$
 CREATE FUNCTION hw_needed_questions(homework_id CHAR(20)) RETURNS INT
 BEGIN
@@ -39,6 +41,8 @@ BEGIN
 END $$
 DELIMITER ;
 
+
+
 DELIMITER $$
 CREATE FUNCTION count_modules(course_id CHAR(10))
 RETURNS INT
@@ -48,6 +52,8 @@ BEGIN
     RETURN count;
 END$$
 DELIMITER ;
+
+
 
 DELIMITER $$
 CREATE FUNCTION mod_question_status(group_id CHAR(20), question_id CHAR(20), student CHAR(9))
@@ -87,6 +93,8 @@ BEGIN
 END$$
 DELIMITER ;
 
+
+
 DELIMITER $$
 CREATE FUNCTION GetCorrectQuestion(
     student_id CHAR(9),
@@ -124,6 +132,7 @@ END $$
 DELIMITER ;
 
 
+
 DELIMITER $$
 CREATE FUNCTION InsertModule(
     module_id CHAR(20),
@@ -153,3 +162,205 @@ BEGIN
 END $$
 DELIMITER ;
 
+
+
+DELIMITER //
+CREATE PROCEDURE ContinuosAttempts(IN student_param CHAR(9), IN homework_param CHAR(20), IN question_param CHAR(20), OUT counter INT)
+BEGIN
+  DECLARE done INT DEFAULT 0;
+  DECLARE attempt_counter INT DEFAULT 0;
+  DECLARE current_question CHAR(20);
+  
+  DECLARE attempts_cursor CURSOR FOR
+    SELECT question
+    FROM hw_questionAttempts
+    WHERE student = student_param AND homework = homework_param
+    ORDER BY attempt_date DESC;
+  
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+  
+  OPEN attempts_cursor;
+  
+  read_loop: LOOP
+    FETCH attempts_cursor INTO current_question;
+    
+    IF done = 1 OR current_question IS NULL OR current_question != question_param THEN 
+      LEAVE read_loop;
+    END IF;
+    
+    SET attempt_counter = attempt_counter + 1;
+  END LOOP;
+  
+  CLOSE attempts_cursor;
+  
+  SET counter = attempt_counter;
+END //
+DELIMITER ;
+
+
+
+DELIMITER $$
+CREATE FUNCTION HomeworkModuleStatus(
+    student_id CHAR(9),
+    homework_id CHAR(20),
+    module_id CHAR(20)
+) RETURNS BOOLEAN
+BEGIN
+    DECLARE needed INT;
+    DECLARE answered INT;
+    DECLARE result BOOLEAN DEFAULT FALSE;
+
+    SELECT n_questions INTO needed
+    FROM homeworkConfigs
+    WHERE homework = homework_id
+    AND module = module_id;
+
+    SELECT COUNT(*) INTO answered
+    FROM hw_questionAttempts hqa
+    JOIN questions q ON hqa.question = q.id_question
+    WHERE hqa.homework = homework_id
+    AND q.module = module_id
+    AND hqa.student = student_id
+    AND hqa.attempt_status = 'PAS';
+
+    IF needed > answered OR needed = 0 OR answered = 0 THEN
+        SET result = FALSE;
+    ELSE
+        SET result = TRUE;
+    END IF;
+
+    RETURN result;
+END$$
+DELIMITER ;
+
+
+
+DELIMITER $$
+CREATE FUNCTION HWQuestionStatus(homework_id CHAR(20), question_id CHAR(20), student_id CHAR(9))
+RETURNS CHAR(3)
+BEGIN
+    DECLARE question_status CHAR(3);
+    DECLARE done BOOLEAN DEFAULT FALSE;
+    
+    DECLARE cur CURSOR FOR
+        SELECT attempt_status
+        FROM hw_questionAttempts
+        WHERE student = student_id
+        AND homework = homework_id
+        AND question = question_id;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    
+    SET question_status = 'PEN';
+
+    OPEN cur;
+        read_loop: LOOP
+        FETCH cur INTO question_status;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+        IF question_status = 'PAS' THEN
+            CLOSE cur;
+            RETURN 'PAS';
+        END IF;
+        END LOOP;
+    CLOSE cur;
+
+    IF question_status = 'PEN' THEN
+        RETURN question_status;
+    ELSE
+        RETURN 'FAI';
+    END IF;
+END$$
+DELIMITER ;
+
+
+
+DELIMITER $$
+CREATE FUNCTION HWModQuestionID(
+    homework_id CHAR(20),
+    module_id CHAR(20),
+    student_id CHAR(9)
+) RETURNS CHAR(20)
+BEGIN
+    DECLARE question CHAR(20);
+
+    SELECT id_question INTO question
+    FROM questions
+    WHERE module = module_id
+    AND HWQuestionStatus(homework_id, id_question, student_id) = 'PEN'
+    ORDER BY RAND()
+    LIMIT 1;
+
+    IF question IS NULL THEN
+        SELECT id_question INTO question
+        FROM questions
+        WHERE module = module_id
+        AND HWQuestionStatus(homework_id, id_question, student_id) = 'FAI'
+        ORDER BY RAND()
+        LIMIT 1;
+    END IF;
+
+    RETURN question;
+END$$
+DELIMITER ;
+
+
+
+DELIMITER $$
+CREATE FUNCTION NewHWQuestionID(
+    homework_id CHAR(20),
+    student_id CHAR(9)
+) RETURNS CHAR(20)
+BEGIN
+    DECLARE moduleToGetHW CHAR(20);
+    DECLARE question CHAR(20);
+
+    SELECT module INTO moduleToGetHW
+    FROM homeworkConfigs 
+    WHERE homework = homework_id
+    AND HomeworkModuleStatus(student_id, homework, module) = 0
+    ORDER BY RAND()
+    LIMIT 1;
+
+    SET question = HWModQuestionID(homework_id, moduleToGetHW, student_id);
+    RETURN question;
+END$$
+DELIMITER ;
+
+
+
+DELIMITER $$
+CREATE FUNCTION HomeworkQuestionID(
+    homework_id CHAR(20),
+    student_id CHAR(9)
+) RETURNS CHAR(20)
+BEGIN
+    DECLARE last_attempt_question CHAR(20);
+    DECLARE last_attempt_status CHAR(3);
+    DECLARE continuousAttempts INT;
+    DECLARE questionID CHAR(20);
+
+    SELECT question, attempt_status INTO last_attempt_question, last_attempt_status
+    FROM hw_questionAttempts
+    WHERE student = student_id
+    AND homework = homework_id
+    ORDER BY attempt_date DESC
+    LIMIT 1;
+
+    IF last_attempt_status = 'PAS' THEN
+        SET questionID = NewHWQuestionID(homework_id, student_id);
+        RETURN questionID;
+    ELSE
+        CALL ContinuosAttempts(student_id, homework_id, last_attempt_question, continuousAttempts);
+
+        IF continuousAttempts < 3 THEN
+            RETURN last_attempt_question;
+        ELSE
+            SET questionID = NewHWQuestionID(homework_id, student_id);
+            RETURN questionID;
+        END IF;
+    END IF;
+
+    RETURN questionID;
+END$$
+DELIMITER ;
