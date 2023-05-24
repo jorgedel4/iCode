a90a075f15fda2fcceb694c259e903a219bc73ba
Karla Sanchez <ksoalianza@gmail.com>
2023-05-23 19:00:19 -0600

    Remove student from group

--- src/client/MindScript/src/app/pages/professor/PManage.jsx
+++ src/client/MindScript/src/app/pages/professor/PManage.jsx
@@ -1,11 +1,20 @@
 import { Grid, useTheme, useMediaQuery, IconButton } from '@mui/material';
 import { useState, useEffect } from 'react';
-import { NavBar, SearchBar } from '../../components';
+import { NavBar, SearchBar, RemoveButton } from '../../components';
 import { DataGrid } from '@mui/x-data-grid';
 import { Delete } from '@mui/icons-material';
 
 export const PManage = () => {
   const batmanAPI = import.meta.env.VITE_APP_BATMAN;
+
+  const [editData, setIdData] = useState(null);
+
+  //Funciones para abrir la modal de Eliminar estudiante
+  const [openRemoveStudent, setOpenRemoveStudent] = useState(false);
+  const showModalRemoveStudent = () => { setOpenRemoveStudent(true); }
+  const closeModalRemoveStudent = () => {
+    setOpenRemoveStudent(false);
+  }
 
   //GET term information
   const [usersData, setUser] = useState([]);
@@ -17,7 +26,7 @@ export const PManage = () => {
       },
       mode: 'cors',
     }
-    
+
 
     // let userID = "A01551955"
     // let term = "current"
@@ -43,14 +52,18 @@ export const PManage = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-        // body: JSON.stringify({ "id": id })
+        body: JSON.stringify({
+          "group": "G000000001",
+          "student": id
+        }),
         mode: 'cors',
 
       };
 
-      const response = await fetch(`http://34.125.0.99:8002/user/${id}`, options);
-      const data = await response.json();
-      return data
+      const response = await fetch(`${batmanAPI}user/unenrollstudent`, options);
+      setUser(prevData => prevData.filter(user => user.id !== id));
+
+      return response;
 
     } catch (error) {
       console.error(error);
@@ -71,12 +84,38 @@ export const PManage = () => {
   const pages = [
     { name: 'Home', route: '/professor/home' },
     { name: 'Profile', route: '/professor/profile' },
-]
+  ]
+
+  const columns = [
+    { field: 'id', headerName: 'Matrícula/Nómina', flex: 2, align: 'center', headerAlign: 'center' },
+    { field: 'name', headerName: 'Nombre', flex: 2, align: 'center', headerAlign: 'center' },
+    {
+      field: 'actions',
+      headerName: 'Acciones',
+      flex: 2,
+      align: 'center',
+      headerAlign: 'center',
+      mx: 10,
+      renderCell: (params) => (
+        <>
+          <IconButton onClick={() => {
+            setIdData(params.row.id);
+            showModalRemoveStudent();
+          }}
+            aria-label="delete" sx={{ color: 'appDark.icon' }}>
+            <Delete />
+          </IconButton>
+        </>
+      ),
+    },
+  ];
 
 
   return (
     <Grid container alignContent='center' justifyContent='center' padding={3} spacing={0} sx={{ minHeight: '100vh', bgcolor: 'primary.main' }}>
-      <NavBar pages={pages}/>
+      <NavBar pages={pages} />
+      < RemoveButton open={openRemoveStudent} close={closeModalRemoveStudent} editData={editData} confirmationText="¿Esta seguro que desea eliminar esta usuario?" handleDelete={handleDelete} />
+
       <Grid container columnSpacing={1} alignItems='center' justifyContent='space-around' sx={{ bgcolor: 'secondary.main', mt: 5, borderRadius: 2, height: containerHeight }}>
         <Grid item xs={12} sm={6} lg={6}>
           <SearchBar searchQuery={nameQuery} name={'Nombre'} placeholder={'Jorge Delgado'} setSearchQuery={setNameQuery} />
@@ -95,27 +134,6 @@ export const PManage = () => {
     </Grid>
   )
 }
-
-const columns = [
-  { field: 'id', headerName: 'Matrícula/Nómina', flex: 2, align: 'center', headerAlign: 'center' },
-  { field: 'name', headerName: 'Nombre', flex: 2, align: 'center', headerAlign: 'center' },
-  {
-    field: 'actions',
-    headerName: 'Acciones',
-    flex: 2,
-    align: 'center',
-    headerAlign: 'center',
-    mx: 10,
-    renderCell: (params) => (
-      <>
-        <IconButton aria-label="delete" sx={{ color: 'appDark.icon' }}>
-          <Delete />
-        </IconButton>
-      </>
-    ),
-  },
-];
-
 
 
 const filterData = (nameQuery, idQuery, usersData) => {
